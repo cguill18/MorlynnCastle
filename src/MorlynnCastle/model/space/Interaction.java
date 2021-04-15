130c9558fd4339752d87e0244fd37df2a4e51b9f
Louis Forestier <louis.forestier@etu.univ-poitiers.fr>
2021-04-15 23:35:26 +0200

    ajout de la position dans Interaction

--- src/MorlynnCastle/model/space/Interaction.java
+++ src/MorlynnCastle/model/space/Interaction.java
@@ -5,13 +5,32 @@ import java.io.Serializable;
 public abstract class Interaction implements Serializable {
 
     private final String description;
+    private int posx;
+    private int posy;
 
     public Interaction(String description) {
         this.description = description;
+    }
+
+    public Interaction(String description, int posx, int posy){
+        this.description = description;
+        this.posx = posx;
+        this.posy = posy;
     }
 
     public void print() {
         System.out.println(this.description);
     }
 
+    public String getDescription(){
+        return this.description;
+    }
+
+    public int getPosx() {
+        return posx;
+    }
+
+    public int getPosy() {
+        return posy;
+    }
 }
