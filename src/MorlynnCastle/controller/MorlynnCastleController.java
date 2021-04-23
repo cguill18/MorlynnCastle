b83f31df159abf85aad4970aecd264cb49753693
Amandine <amandine.fradet@live.fr>
2021-04-23 16:25:52 +0200

    On peut fermer le coffre et le rouvrir
    C'est le machin.getWindow qui fait tout boguer donc il reste en commentaire pour l'instant

--- src/MorlynnCastle/controller/MorlynnCastleController.java
+++ src/MorlynnCastle/controller/MorlynnCastleController.java
@@ -99,6 +99,7 @@ public class MorlynnCastleController {
         this.directionPaneController.setGame(this.game);
         this.directionPaneController.setMorlynnCastleController(this);
         this.gridPaneRoot.styleProperty().bind(Bindings.concat("-fx-font-size:", gridPaneRoot.widthProperty().divide(60).asString(), ";", gridPaneRoot.getStyle()));
+        //this.containerstage.initOwner(this.gridPaneRoot.getScene().getWindow());
     }
 
     public Game getGame() {
@@ -132,7 +133,7 @@ public class MorlynnCastleController {
                     this.containerPaneController.setContainerLooking((Container) interaction);
                     this.containerPaneController.displayContainer(((Container) interaction).getContent());
                     this.containerstage.setTitle("Containts of the chest");
-                    this.containerstage.initOwner(this.gridPaneRoot.getScene().getWindow());
+                    
                     this.containerstage.show();
                 }
                 break;
