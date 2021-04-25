3111ae09bb4247320e1ac787abbfb79995f866ee
Louis Forestier <louis.forestier@etu.univ-poitiers.fr>
2021-04-25 11:32:20 +0200

    on comprend enfin qu'il faut cliqué sur les dialogues

--- src/MorlynnCastle/controller/DialogBoxController.java
+++ src/MorlynnCastle/controller/DialogBoxController.java
@@ -3,7 +3,9 @@ package MorlynnCastle.controller;
 import MorlynnCastle.model.characters.Dialog;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
 import javafx.scene.control.TextArea;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
@@ -36,6 +38,14 @@ public class DialogBoxController {
 
     public void addDialog(String text, EventHandler answer){
         Text line = new Text(text+"\n");
+        line.setOnMouseEntered(event -> {
+            line.getScene().setCursor(Cursor.HAND);
+            line.setFill(Color.CADETBLUE);
+        });
+        line.setOnMouseExited(event -> {
+            line.getScene().setCursor(Cursor.DEFAULT);
+            line.setFill(Color.BLACK);
+        });
         this.textFlow.getChildren().add(line);
         line.setOnMouseClicked(answer);
     }
